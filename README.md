93aee5951e5414861daa9c8135c006b994399039
Robert <r.raynsford@gmail.com>
2023-06-18 15:07:02 +0100

    feat: initial blog code

---
+++ README.md
@@ -0,0 +1,58 @@
+# My Personal Blog
+
+This repository contains the code for my blog, which is built using [Astro](https://astro.build/), a static site builder. The blog posts are written in the .mdx format, which combines Markdown and JSX.
+
+## 👀 Visit
+
+To visit my blog, go to https://rrayns.co.uk.
+
+## ✉️ Contact
+
+Feel free to reach out to me. You can contact me via email at [robert@rrayns.co.uk](mailto:robert@rrayns.co.uk).
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ astro.config.mjs
@@ -0,0 +1,32 @@
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
+import mdx from "@astrojs/mdx";
+
+// https://astro.build/config
+import sitemap from "@astrojs/sitemap";
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://rrayns.co.uk',
+  integrations: [tailwind(), mdx(), sitemap()],
+  markdown: {
+    shikiConfig: {
+      // Choose from Shiki's built-in themes (or add your own)
+      // https://github.com/shikijs/shiki/blob/main/docs/themes.md
+      theme: 'css-variables'
+    }
+  },
+  vite: {
+    build: {
+      rollupOptions: {
+        output: {
+          assetFileNames: 'assets/[hash][extname]',
+        },
+      },
+    },
+  },
+});
